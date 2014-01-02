57e73797660d0a0a109009827ba00096183ded5a
Derek Rockwell <derek.a.rockwell@gmail.com>
2014-01-02 14:06:08 -0600

    fixes

--- lib/language_pack/rails3.rb
+++ lib/language_pack/rails3.rb
@@ -58,7 +58,9 @@ class LanguagePack::Rails3 < LanguagePack::Rails2
     instrument "rails3.run_assets_precompile_rake_task" do
       log("assets_precompile") do
         setup_database_url_env
-        return true unless rake_task_defined?("assets:precompile")
+
+        precompile = rake.task("assets:precompile")
+        return true unless precompile.is_defined?
 
         topic("Preparing app for Rails asset pipeline")
         if File.exists?("public/assets/manifest.yml")
@@ -74,11 +76,11 @@ class LanguagePack::Rails3 < LanguagePack::Rails2
 
         puts "Running: rake assets:precompile"
         require 'benchmark'
-        time = Benchmark.realtime { pipe("env PATH=$PATH:bin bundle exec rake assets:precompile 2>&1") }
 
-        if $?.success?
+        precompile.invoke
+        if precompile.success?
           log "assets_precompile", :status => "success"
-          puts "Asset precompilation completed (#{"%.2f" % time}s)"
+          puts "Asset precompilation completed (#{"%.2f" % precompile.time}s)"
 
           # If 'turbo-sprockets-rails3' gem is available, run 'assets:clean_expired' and
           # cache assets if task was successful.
